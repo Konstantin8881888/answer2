fcac6a12e6e01e6b47cd31445ac1084b7b6ff16a
Konstantin <kamagoroff@gmail.com>
2023-03-17 16:34:55 +0300

    Initialized files

---
+++ src/main/java/com/example/answer2/Answer2Application.java
@@ -0,0 +1,13 @@
+package com.example.answer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Answer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Answer2Application.class, args);
+	}
+
+}
